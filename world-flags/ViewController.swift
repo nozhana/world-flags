b625d936a476cf89a93a44276813a954663411ba
Nozhan Amiri <nozhana@gmail.com>
2022-04-23 20:29:29 +0430

    Initial Commit

---
+++ world-flags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  world-flags
+//
+//  Created by Nozhan Amiri on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
