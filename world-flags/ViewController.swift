533ba6eb21c14b3806c862d8b871ddd019aec058
Nozhan Amiri <nozhana@gmail.com>
2022-04-24 10:15:39 +0430

    Implement tableView and add countries Implement modern content configuration rather than text and image properties

--- world-flags/ViewController.swift
+++ world-flags/ViewController.swift
@@ -7,11 +7,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIImage {
+    /// Scales down a UIImage by `coefficient`
+    ///
+    /// This function is an extension of `UIImage`.
+    ///
+    /// Implementation
+    /// ```swift
+    /// resizedImage = image.scaledDown(by: 0.5)
+    /// ```
+    ///
+    /// - Parameter coefficient: `CGFloat` by which the `UIImage` object is scaled down
+    /// - Returns: A `UIImage?` optional containing the new resized image
+    func scaledDown(by coefficient: CGFloat) -> UIImage? {
+        guard coefficient >= 0 && coefficient <= 1 else {
+            print("Coefficient must be a CGFloat in 0...1")
+            return nil
+        }
+        
+        let newWidth = size.width * coefficient
+        let newHeight = size.height * coefficient
+        
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        
+        draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
 
+class ViewController: UITableViewController {
+    
+    /// String array containing the name of countries to work with.
+    ///
+    /// Property of ``ViewController``
+    ///
+    /// Assigned in ``viewDidLoad()``
+    var countries = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Countries"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+//        Add ...@3x.png files to countries and drop the text after country name "@3x.png"
+        countries = items.filter{$0.hasSuffix("@3x.png")}.compactMap{$0.components(separatedBy: "@").first}
+        
+        print(countries) // MARK: Check if filenames are retrieved correctly
+        
+    }
+    
+    /// Determine number of rows displayed in `tableView`
+    ///
+    /// Overriding `tableView`
+    /// - Parameters:
+    ///   - tableView: tableView reference object. Usually `UITableView`.
+    ///   - section: The section in which number of rows are being set.
+    /// - Returns: The number of rows in the designated section.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let country = countries[indexPath.row]
+        
+        content.text = (country == "uk" || country == "us") ? country.uppercased() : country.capitalized
+        content.image = UIImage(named: country)?.scaledDown(by: 0.5)!
+        
+        cell.contentConfiguration = content
+        return cell
     }
 
 
